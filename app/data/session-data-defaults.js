b316d79054e9ba4b59d40a7f884459af41b59b09
scachb <136708737+scachb@users.noreply.github.com>
2023-10-10 18:27:52 +0100

    Passing the data to Check answers
    Added a join for the date of birth thanks Ed

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,14 @@
 module.exports = {
 
   // Insert values here
-
+  'nino-data': 'WE896405D',
+  'changed-name': 'Yes',
+  'known-name':'Yes',
+  'other-last-names':'Jonas',
+  'dateOfBirth':'21 03 1975',
+  'confirm-address':'Yes',
+  'address-over-year':'Yes',
+  'tel-number':'07700900982',
+  'nationality-data':'British',
+  'nationality-another':'No'
 }

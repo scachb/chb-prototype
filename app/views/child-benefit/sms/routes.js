7318d35d8a30229686a2f2d5664d9f8213b5b56a
scachb <136708737+scachb@users.noreply.github.com>
2023-11-28 21:39:35 +0000

    SMS mocks

--- app/views/child-benefit/sms/routes.js
+++ app/views/child-benefit/sms/routes.js
@@ -4,11 +4,19 @@
 //
 
 const govukPrototypeKit = require('govuk-prototype-kit')
-
 const smsRouter = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
-
+// Conditional routing for address over the other name
+smsRouter.post('/sms/your-telephone-number', function(request, response) {
+    var receiveSMS = request.session.data['receiveSMS']
+    if (request.session.data['child-benefit']['sms']['receiveSMS'] == "Yes"){
+        response.redirect("/child-benefit/sms/confirm-a-phone-number")
+    } else {
+        response.redirect("/child-benefit/sms/question-page")
+    }  
+  
+  })
 
 // Export the journey router to be used by the service
 module.exports = smsRouter;

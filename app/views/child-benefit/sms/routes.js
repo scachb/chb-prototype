1d1c33b5ba2ce830439bcbe5fe533b4a966e261f
ordyboii <jake.ord345@gmail.com>
2023-11-30 14:36:28 +0000

    updated pages

--- app/views/child-benefit/sms/routes.js
+++ app/views/child-benefit/sms/routes.js
@@ -21,23 +21,23 @@ const smsRouter = govukPrototypeKit.requests.setupRouter();
 
 // The URL here needs to match the URL of the page that the user is on
 // when they type in their email address
-smsRouter.post('/email-address-page', function (req, res) {
+// smsRouter.post('/email-address-page', function (req, res) {
 
-    notify.sendEmail(
-      // this long string is the template ID, copy it from the template
-      // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-      // in your code.
-      'eb4dabb6-5685-4526-9a4d-c856ae108762',
-      // `emailAddress` here needs to match the name of the form field in
-      // your HTML page
-      req.body.emailAddress
-    );
+//     notify.sendEmail(
+//       // this long string is the template ID, copy it from the template
+//       // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+//       // in your code.
+//       'eb4dabb6-5685-4526-9a4d-c856ae108762',
+//       // `emailAddress` here needs to match the name of the form field in
+//       // your HTML page
+//       req.body.emailAddress
+//     );
   
-    // This is the URL the users will be redirected to once the email
-    // has been sent
-    res.redirect('/confirmation-page');
+//     // This is the URL the users will be redirected to once the email
+//     // has been sent
+//     res.redirect('/confirmation-page');
   
-  });
+//   });
 
 
 // Export the journey router to be used by the service

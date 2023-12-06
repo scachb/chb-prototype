46e7567a3eb298a03df4009070e9f347d290febf
scachb <136708737+scachb@users.noreply.github.com>
2023-12-06 14:17:31 +0000

    PA design iterations
    PT personal taxpayer

--- app/views/child-benefit/routes.js
+++ app/views/child-benefit/routes.js
@@ -1,14 +1,12 @@
 //
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
-//
 
 const govukPrototypeKit = require('govuk-prototype-kit');
 const endToEndRouter = require('./end-to-end/routes');
 const portalRouter = require('./portal/routes');
 const proofOfEntitlementRouter = require('./proof-of-entitlement/routes');
 const unauthRouter = require('./unauth/routes');
-const smsRouter = require('./sms/routes');
 
 const childBenefitRouter = govukPrototypeKit.requests.setupRouter();
 
@@ -25,9 +23,6 @@ childBenefitRouter.use('/proof-of-entitlement', proofOfEntitlementRouter);
 
 // Registers all routes to /child-benefit/unauth
 childBenefitRouter.use('/unauth', unauthRouter);
-
-// Registers all routes to /child-benefit/unauth
-childBenefitRouter.use('/sms', smsRouter);
 
 // Export the journey router to be used by the service
 module.exports = childBenefitRouter;

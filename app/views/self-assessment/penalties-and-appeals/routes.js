97db21216a9b073faaa296ea3640ed12b3d8a994
scachb <136708737+scachb@users.noreply.github.com>
2023-12-06 17:59:53 +0000

    Credit to jake for helping with routes

--- app/views/self-assessment/penalties-and-appeals/routes.js
+++ app/views/self-assessment/penalties-and-appeals/routes.js
@@ -9,5 +9,16 @@ const penaltiesAndAppealsRouter = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
 
+penaltiesAndAppealsRouter.post(
+    '/reason-answer', 
+    function(request, response) {
+      if (request.session.data['self-assessment']['penalties-and-appeals']['reasonLateFiling'] == "other-reason"){
+        response.redirect("./other-filing")
+      } else {
+        response.redirect("./check-answers")
+      }
+    }
+  )
+
 // Export the journey router to be used by the service
 module.exports = penaltiesAndAppealsRouter;

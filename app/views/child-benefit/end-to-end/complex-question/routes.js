cac0388fafe37c2a877e92e9dc40fb3183b0862f
scachb <136708737+scachb@users.noreply.github.com>
2023-12-14 16:48:18 +0000

    Added is your current address

--- app/views/child-benefit/end-to-end/complex-question/routes.js
+++ app/views/child-benefit/end-to-end/complex-question/routes.js
@@ -13,8 +13,7 @@ complexQuestionRouter.post('/confirm-lived-answer', function(request, response) 
   var livedUKAbroad = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-address-data']
   var LivedUK = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-address-data']
   var NeverLivedUK = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-address-data']
-  
-  
+
   if (livedUKAbroad == "livedUKAbroad"){
       response.redirect("./lived-uk-abroad")
   } else if (LivedUK == "LivedUK") {
@@ -23,7 +22,20 @@ complexQuestionRouter.post('/confirm-lived-answer', function(request, response) 
   else if (NeverLivedUK == "NeverLivedUK") {
     response.redirect("./country-normally-live")
   }
+  
 
+})
+
+
+complexQuestionRouter.post('/confirm-lived-ukaborad-answer', function(request, response) {
+
+  var livedUKAbroadNormally = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-uk-data']
+  
+  if (livedUKAbroadNormally == "yes"){
+      response.redirect("./is-your-current-address-in-uk")
+  } else {
+    response.redirect("./question-page")
+  }
 
 })
 

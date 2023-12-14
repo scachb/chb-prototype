4d034040af1b85ccacdec6a992b49245dd4710bf
scachb <136708737+scachb@users.noreply.github.com>
2023-12-14 15:44:01 +0000

    Further updates to the complex question

--- app/views/child-benefit/end-to-end/complex-question/routes.js
+++ app/views/child-benefit/end-to-end/complex-question/routes.js
@@ -11,12 +11,22 @@ const complexQuestionRouter = govukPrototypeKit.requests.setupRouter();
 complexQuestionRouter.post('/confirm-lived-answer', function(request, response) {
 
   var livedUKAbroad = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-address-data']
+  var LivedUK = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-address-data']
+  var NeverLivedUK = request.session.data['child-benefit']['end-to-end']['complex-question']['details-lived-address-data']
+  
+  
   if (livedUKAbroad == "livedUKAbroad"){
       response.redirect("./lived-uk-abroad")
-  } else {
-      response.redirect("./question-page")
+  } else if (LivedUK == "LivedUK") {
+    response.redirect("./current-uk-address")
   }
+  else if (NeverLivedUK == "NeverLivedUK") {
+    response.redirect("./country-normally-live")
+  }
+
+
 })
+
 
 
 // Export the journey router to be used by the service

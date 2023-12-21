caab68b04dbb7a6d7b135e70c14f5d3547dee64c
Jake (CDIO) Ord <68291857+ordyboii@users.noreply.github.com>
2023-12-21 15:57:19 +0000

    new

--- app/aui/kit.jsx
+++ app/aui/kit.jsx
@@ -108,7 +108,7 @@ export function HomePage({ journeys }) {
       </header>
 
       <div className="govuk-width-container">
-        <main className="govuk-main-wrapper " id="main-content" role="main">
+        <main className="govuk-main-wrapper" id="main-content" role="main">
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-two-thirds">
               <h1 className="govuk-heading-l govuk-!-margin-top-6">
